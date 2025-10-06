7a3c99960ac31d39d978cf692e74dab80354f599
Alessandro Rios <drios28@outlook.es>
2025-10-06 12:50:35 -0500

    Elimina el archivo de ejemplo de variables de entorno y actualiza la URL de la aplicación en las constantes.

--- src/core/lib/constants/index.ts
+++ src/core/lib/constants/index.ts
@@ -3,7 +3,7 @@ export const APP_NAME = 'FinPath'
 export const APP_DESCRIPTION = 'FinPath es una plataforma digital de educación financiera que te ayuda a entender, organizar y mejorar tus finanzas personales. A través de herramientas interactivas, cursos personalizados y un diagnóstico financiero inteligente, te guiamos paso a paso para construir hábitos sólidos, mejorar tu historial crediticio y alcanzar tus metas económicas.'
 export const APP_SLOGAN = 'Tu camino hacia la libertad financiera'
 
-export const APP_URL = 'https://finpath.vercel.app'
+export const APP_URL = 'http://finpath-efsrtv.vercel.app'
 
 //* AUTH
 export const USER_ROLES = { ADMIN: 'admin', USER: 'user' } as const
